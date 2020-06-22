2fe193e5d458c50c3554e0f83273e5629fc3dff7
ming <1498890331@qq.com>
2020-06-22 14:39:51 +0800

    no message

--- scripts/test_network.py
+++ scripts/test_network.py
@@ -47,5 +47,10 @@ class TestNetwork:
         self.network_page.click_first_network()
         self.network_page.click_3g_network()
 
+    def test_mobile_network_7g(self):
+        self.network_page.click_more()
+        self.network_page.click_network()
+        self.network_page.click_first_network()
+        self.network_page.click_3g_network()
 # 1
 # 2
